3eb3b9a0747d6a4744e76d6b09d3362313434c84
tooneaa <tooneaa@hotmail.com>
2019-10-05 21:09:50 +0800

    added invalid and emprty error, added ajax functionality

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,8 +1,16 @@
 class StocksController < ApplicationController
 
   def search
-    @stock = Stock.new_from_lookup(params[:stock])
-    render 'users/my_portfolio'
+    if params[:stock].blank?
+      flash.now[:danger]= "You have entered an empty search"
+    else
+      @stock = Stock.new_from_lookup(params[:stock])
+      flash.now[:danger] = "You have entered a wrong ticker symbol" unless @stock
+      end
+
+    respond_to do |format|
+    format.js{render partial:'users/results'}
+    end
   end
 
 end
